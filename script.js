ba7c5597c20557e412f4b4e44aa3d91cd0d68626
David Helberg <helberg.david@gmail.com>
2017-06-07 00:57:20 +0000

    Hello Ziv

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 var winner = winner;
 var turn = turn;
 
-function pickTurn(){
-    document.querySelector('#clickme').addEventListener('click',buttonClicked);
-    document.querySelector('#aDiv').addEventListener('click',buttonClicked);
+function onLoad(){
+    document.querySelector('#clickme').addEventListener('click',pickTurn);
+    document.querySelector('#aDiv').addEventListener('click',pickTurn);
+}
+
+function pickTurn(event){
+    if(event.target.id=='Xbutton'){
+        alert ("X was chosen");
+    }
 }
